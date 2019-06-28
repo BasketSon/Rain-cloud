9e82da54aac3326b4e79c623ac1b24d3e9e154c8
Basket Son <ehal.bydlo@gmail.com>
2019-06-28 11:56:53 +0300

    Initial commit

---
+++ app/js/main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+(function () {
+  var setup = function () {
+    var rainDrops = 500;
+    var canvas = document.querySelector('.canvas');
+    var ctx = canvas.getContext('2d');
+    ctx.fillStyle = 'red';
+    console.log(canvas);
+
+  }
+
+})();
